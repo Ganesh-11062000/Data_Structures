c4ea5ecf4fd6d3d2d3457f4deb9c680f05ec0d2c
Ganesh Moota <ganeshnm2000@gmail.com>
2020-04-26 18:38:18 +0530

    Implementation of Stack

--- Stack/Stack.c
+++ Stack/Stack.c
@@ -7,6 +7,109 @@ typedef struct Stack{
     int tos;
 }Stack;
 
-void main(){
+int isEmpty(Stack *t){
+    if(t->tos == -1){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+void display(Stack *t){
+    if(isEmpty(t)==1){
+        printf("Stack is empty \n");
+        return;
+    }else{
+        for(int i=t->tos;i>=0;i--){
+            printf("%d ",t->S[i]);
+        }
+        printf("\n");
+    }
+
+}
+
+void push(Stack *t,int ele){
+
+    if(t->tos == MAX -1){
+        printf("Stack overflow \n");
+    }else{
+        t->tos++;
+        t->S[t->tos] = ele;
+        display(t);
+    }
     
+}
+
+int pop(Stack *t){
+    int ele;
+
+    if(isEmpty(t)==1){
+        printf("Stack underflow \n");
+        return -1;
+    }else{
+        ele = t->S[t->tos];
+        t->tos--;
+
+        display(t);
+
+        return ele;
+    }
+
+}
+
+int StackTop(Stack *t){
+    int ele;
+
+    if(isEmpty(t)==1){
+        printf("Stack is empty \n");
+        return -1;
+    }else{
+        ele = t->S[t->tos];
+
+        printf("%d\n",ele);
+
+        return ele;
+    }
+}
+
+
+void main(){
+    Stack x;
+    x.tos = -1;
+
+    int ch,ele;
+
+    while(1){
+        
+        printf("Enter 1:Push 2:Pop 3:StackTop 4:Display 5:Exit\n");
+        scanf("%d",&ch);
+
+        if(ch==5){
+            break;
+        }
+
+        switch (ch)
+        {
+            case 1:
+                printf("Enter an integer value - ");
+                scanf("%d",&ele);
+                push(&x,ele);
+                break;
+            case 2:
+                pop(&x);
+                break;
+            case 3:
+                StackTop(&x);
+                break;
+            case 4:
+                display(&x);
+                break;
+            default: printf("Invalid choice\n");
+
+        }
+
+        printf("\n---------------------------------------------\n");
+    }
+
+
 }
